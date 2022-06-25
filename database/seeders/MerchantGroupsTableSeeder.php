842d2a308166bae0c71b2b5f0aa90db89b664f1a
airondev <airondev@gmail.com>
2022-06-25 12:13:04 +0100

    heroku deploy

--- database/seeders/MerchantGroupsTableSeeder.php
+++ database/seeders/MerchantGroupsTableSeeder.php
@@ -14,7 +14,7 @@ class MerchantGroupsTableSeeder extends Seeder
      */
     public function run()
     {
-        // MerchantGroup::truncate();
+        MerchantGroup::truncate();
         MerchantGroup::insert([
             ['id' => 1, 'name' => 'Premium', 'description' => 'This is the premium merchant group', 'fee' => 0.51234567, 'is_default' => 'No'],
             ['id' => 2, 'name' => 'Gold', 'description' => 'This is the gold merchant group', 'fee' => 1.99933344, 'is_default' => 'No'],
